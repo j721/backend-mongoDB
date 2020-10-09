0d78b9655673f78dd3d40cf62a542454b12f4fb5
j721 <juliadvu@gmail.com>
2020-10-09 01:48:12 -0500

    Chore: reformat for better readability

--- models/db.js
+++ models/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/UserDB',{
-    useNewUrlParser: true
-},
-err=>{
-    if(!err){
-        console.log('Connection successful')
-    }else{
-        console.log('Error in connection', err)
+mongoose.connect(
+  "mongodb://localhost:27017/UserDB",
+  {
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("Connection successful");
+    } else {
+      console.log("Error in connection", err);
     }
-});
+  }
+);
 
-require('./user.model');
+require("./user.model");
