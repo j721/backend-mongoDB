272710489d014f0fa83516488ed49a0897282d63
j721 <juliadvu@gmail.com>
2020-10-09 01:47:12 -0500

    Chore: re-formatted for easier readability

--- api/server.js
+++ api/server.js
@@ -13,9 +13,9 @@ server.use(bodyparser.json());
 
 server.get('/', (req,res)=>{
     res.send(`
-    <h2>Here is Your Dashboard! </h2>
-    <h3>Click here to have access to your 
-    <b> <a href ="/user/list"> Subscriptions </a></b>
+    <h2>Welcome to User Database! </h2>
+    <h3>Click here to have access to the
+    <b> <a href ="/user/list"> Database </a></b>
     </h3>
     `)
 });
