3876c1da93714c8141ff2792b3660c5b9c102163
j721 <juliadvu@gmail.com>
2020-10-09 01:46:40 -0500

    Chore: re-formatted code to look cleaner

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
