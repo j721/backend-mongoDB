0f0101f5f823f1d18716b394369d1d246f33f8b4
j721 <juliadvu@gmail.com>
2020-10-09 01:47:50 -0500

    Feat: creating update and edit functions for User

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const { unregisterPartial } = require("handlebars");
+//import from UserModel
+const User = mongoose.model("User");
+
+router.get("/", (req, res) => {
+  res.render("user/addOrEdit", {
+    viewTitle: "Insert User",
+  });
+});
+
+router.post("/", (req, res) => {
+  if (req.body._id == "") {
+    insertRecord(req, res);
+  } else {
+    updateRecord(req, res);
+  }
+});
+
+function insertRecord(req, res) {
+  var user = new User();
+  user.fullName = req.body.fullName;
+  user.email = req.body.email;
+  user.mobile = req.body.mobile;
+  user.city = req.body.city;
+
+  user.save((err, doc) => {
+    if (!err) {
+      res.redirect("user/list");
+    } else {
+      console.log("Error when inserting record", err);
+    }
+  });
+}
+
+function updateRecord(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.body._id },
+    req.body,
+    { new: true },
+    (err, doc) => {
+      if (!err) {
+        res.redirect("user/list");
+      } else {
+        console.log("Error during update", update);
+      }
+    }
+  );
+}
