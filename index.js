2547cb2babc2506151eed7f2ebf181b538a270a7
j721 <juliadvu@gmail.com>
2020-10-08 23:44:17 -0500

    installed dependencies needed to be used in the index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const path = require('path')
+const handlebars =require('handlebars')
+const exphbs = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const bodyparser = require('body-parser')
