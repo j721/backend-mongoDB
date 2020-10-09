ba65a88b0d27158ce6d89383c74577380fec6b18
j721 <juliadvu@gmail.com>
2020-10-09 01:31:57 -0500

    Feat: setting up mongoDB databse with mongoose

--- models/db.js
+++ models/db.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/UserDB',{
+    useNewUrlParser: true
+},
+err=>{
+    if(!err){
+        console.log('Connection successful')
+    }else{
+        console.log('Error in connection', err)
+    }
+});
+
+require('./user.model');
