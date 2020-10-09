c420d45444c121ec836883e5e1cf30caa363b795
j721 <juliadvu@gmail.com>
2020-10-08 23:56:53 -0500

    Feat: created initial port

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const handlebars =require('handlebars')
 const exphbs = require('express-handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const bodyparser = require('body-parser')
+
+var app = express()
+ 
+app.use(bodyparser.urlencoded{extended: false})
+app.use(bodyparser.json())
+
+app.listen(3000, ()=>{
+    console.log('server running on port 3000')
+})
