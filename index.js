62a50d8cf6ad1ac51e3fe865b61d4e604767ae89
j721 <juliadvu@gmail.com>
2020-10-12 22:28:46 -0500

    Updated index.js to handle all of the server routes and the port to listen from

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const server = require("./api/app");
+require("./models/db");
 
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const path = require("path");
+const handlebars = require("handlebars");
+const exphbs = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const bodyparser = require("body-parser");
 
-server.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+const userController = require("./controllers/userController");
+
+var app = express();
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h2>Welcome to User Database! </h2>
+    <h3>Click here to have access to the
+    <b> <a href ="/user/list"> Database </a></b>
+    </h3>
+    `);
 });
+
+app.set("views", path.join(__dirname, "/views/"));
+
+app.engine(
+  "hbs",
+  exphbs({
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+    extname: "hbs",
+    defaultLayout: "MainLayout",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
+
+app.set("view engine", "hbs"); //hbs stands for handlebars
+
+app.listen(3000, () => {
+  console.log("it's working!");
+});
+
+app.use("/user", userController);
+
+
